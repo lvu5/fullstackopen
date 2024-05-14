3bc0cfde4b2bcfc1dc91dcb999e37dc329d8fbb9
lvu5 <lvu5@uic.edu>
2024-05-14 11:26:37 -0500

    done 1.2

--- part1/courseinformation/src/App.jsx
+++ part1/courseinformation/src/App.jsx
@@ -8,9 +8,18 @@ const App = () => {
     )
   }
 
+  const Part = (props) => {
+    return(
+      <p>{props.part} {props.exercises}</p>
+    )
+  }
   const Content = (props) => {
     return (
-      <p>{props.part} {props.exercises}</p>
+      <div>
+        <Part part={props.part1} exercises={props.exercises1}></Part>
+        <Part part={props.part2} exercises={props.exercises2}></Part>
+        <Part part={props.part3} exercises={props.exercises3}></Part>
+      </div>
     )
   }
 
@@ -32,9 +41,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}></Header>
-      <Content part={part1} exercises={exercises1}></Content>
-      <Content part={part2} exercises={exercises2}></Content>
-      <Content part={part3} exercises={exercises3}></Content>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}></Content>
       <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}></Total>
     </div>
   )
